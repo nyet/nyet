9f701dde1ed2482b82b10618ef153c4397995662
Leo Susanto <leosusanto@gmail.com>
2012-02-19 23:39:44 -0800

    add new perl coverage

---
+++ projects/CPANTesters/perl_coverage.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+use strict;
+
+my $running = 1;
+while ($running) {
+   foreach my $platform (@platforms) {
+      #open SQLite database
+      # get the next dev-*.txt file for $platform from SQLite.
+      # close SQLite database
+      # if there is no more dev-*.txt file for that platform
+         $running = 0;
+      
+}
