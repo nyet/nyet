b1d2e84187ed0e38a0d6091590b1c421dfda4a88
Leo Susanto <leosusanto@gmail.com>
2012-09-27 23:43:12 -0700

    Learning DMV

--- PerformanceTest/DMV.sql
+++ PerformanceTest/DMV.sql
@@ -1,5 +1,7 @@
 SELECT name, type_desc FROM sys.system_objects WHERE name LIKE 'dm_%' ORDER BY name
 
+Page #17: 1.1.1 A glimpse into SQL Server’s internal data
+
 SELECT * FROM sys.dm_exec_requests
 sql_handle: 0x0200000002C6D42F7AAACE541C95D637FEB5A6185258B435
 
@@ -12,3 +14,8 @@ query_plan_hash: 0xAD412BC185253225
 
 dm_exec_sql_text	SQL_INLINE_TABLE_VALUED_FUNCTION
 
+
+sys.dm_exec_sql_text(sql_handle)
+
+
+Page #20: 1.2.2 Performance tuning
