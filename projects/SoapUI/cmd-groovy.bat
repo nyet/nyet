82ac70d2c28ed46f877f2698c9b58243eaaafdaf
Leo Susanto <leosusanto@gmail.com>
2012-10-24 19:20:02 -0700

    SoapUI POC

---
+++ projects/SoapUI/cmd-groovy.bat
@@ -0,0 +1,8 @@
+SET PATH=C:\work\bin\PortableApps\CommonFiles\Java\bin;%PATH%
+SET PATH=C:\Documents and Settings\vmuser\Desktop\groovy-2.0.5\bin;%PATH%
+SET JAVA_HOME=C:\work\bin\PortableApps\CommonFiles\Java
+SET GROOVY_HOME=C:\Documents and Settings\vmuser\Desktop\groovy-2.0.5
+
+REM cd C:\CPANTesters\testdevcode
+REM start "Groovy" CMD /K
+groovyConsole.bat
